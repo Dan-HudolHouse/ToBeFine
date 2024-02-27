adc238a7596ef8ff96750c9c175a4a51dc482cfd
dj-roberts <daniel@hudolhouse.com>
2024-02-27 21:14:41 +1100

    MenuScene draft completed

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -7,7 +7,7 @@ using Cinemachine;
 
 public class MainMenu : MonoBehaviour
 {
-    public CinemachineVirtualCamera splashScreenCamera, menuCamera;
+    public CinemachineVirtualCamera splashScreenCamera, menuCamera, finalPos;
     public GameObject ilight;
     private bool canInteract, isActivated;
 
@@ -33,10 +33,14 @@ public class MainMenu : MonoBehaviour
     {
         canInteract = true;
     }
+    public void TransitionToLevel()
+    {
+        StartCoroutine(FadeToGame());
+    }
     IEnumerator TransitionToMenu()
     {
         ilight.SetActive(false);
-        isActivated = false;
+        isActivated = true;
 
         logoFade.fadeMode = UIFadeIn.mode.FadeOut;
         logoFade.duration = 1;
@@ -47,6 +51,22 @@ public class MainMenu : MonoBehaviour
         splashScreenCamera.Priority = 9;
 
         menuFade.BeginFade();
+        yield return null;
+    }
+    IEnumerator FadeToGame()
+    {
+
+
+        menuFade.fadeMode = UIFadeIn.mode.FadeOut;
+        menuFade.startDelay = 0;
+        menuFade.duration = 1;
+        menuFade.BeginFade();
+        
+        finalPos.Priority = 13;
+        splashScreenCamera.Priority = 9;
+        menuCamera.Priority = 9;
+
+        
         yield return null;
     }
 }
